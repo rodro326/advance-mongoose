47a1c60ea8e69b397616e84fd62f2ea0b48974fe
Rodro <rodro@Rodros-MacBook-Air.local>
2025-05-14 17:29:52 +0600

    use

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
-const express = require('express')
-const app = express()
+import express, { Application, Request, Response } from 'express'
+import cors from "cors"
+const app: Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+// parsers
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
-const mongoose = require('mongoose');
+import config from "./app/config";
+import mongoose from "mongoose";
+import app from './app'
 
 
-
+// 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  try{
+    await mongoose.connect(config.databse_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  }
+  catch(err){
+    console.log(err)
+  }
+ 
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
